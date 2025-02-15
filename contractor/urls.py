d19f83bf698b90e9fc4b111bd43e5a15421fb5a6
Azharudeen M <fatima.anish@gmail.com>
2025-02-15 15:15:56 +0000

    Resolve url pattern issue for contract profile

--- contractor/urls.py
+++ contractor/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    #path('<int:contractor_id>/', views.contractor_profile, name='contractor_profile'),
+    path('<int:contractor_id>/', views.contractor_profile, name='contractor_profile'),
     path("<int:user_id>/", views.contractor_detail, name="contractordetail"),
     path('become-contractor/', views.become_contractor, name='become_contractor'),
 ]
