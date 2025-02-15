d868297bc77f04b90c9edf1ebdf947475296896b
Azharudeen M <fatima.anish@gmail.com>
2025-02-15 15:20:08 +0000

    test

--- contractor/urls.py
+++ contractor/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path('<int:contractor_id>/', views.contractor_profile, name='contractor_profile'),
-    path("<int:user_id>/", views.contractor_detail, name="contractordetail"),
+    path("<int:user_id>/", views.contractor_detail, name="contractordetail"), #Added by Anish
     path('become-contractor/', views.become_contractor, name='become_contractor'),
 ]
