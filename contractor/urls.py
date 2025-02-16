80c74b66df0914d24324661d4fda4110d6df3387
Azharudeen M <fatima.anish@gmail.com>
2025-02-16 22:05:45 +0000

    Add contract-list page

--- contractor/urls.py
+++ contractor/urls.py
@@ -4,10 +4,10 @@ from . import views
 
 urlpatterns = [
     #path('<int:contractor_id>/', views.contractor_profile, name='contractor_profile'),
-    path("<int:user_id>/", views.contractor_detail, name="contractordetail"), #Added by Anish
+    path("<int:user_id>/", views.contractor_detail, name="contractor_detail"), #Added by Anish
     path('become-contractor/', views.become_contractor, name='become_contractor'),
     path('contractor/<slug:slug>/edit_review/<int:review_id>/', views.review_edit, name='review_edit'),
     path('contractor/<int:review_id>/edit/', views.review_edit, name='edit_review'),
     path('review/<int:review_id>/delete/', views.review_delete, name='delete_review'),
-   
+    path('contractors/', views.ContractorList.as_view(), name='contractor_list'),
 ]
