942584b322c47182d2b125bfc75c4f3e6f2345a6
Isaac Nicholls <isaac.nicholls1@gmail.com>
2025-02-17 09:55:24 +0000

    enable serving of static files

--- jobsearch/settings.py
+++ jobsearch/settings.py
@@ -67,6 +67,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'allauth.account.middleware.AccountMiddleware'
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'jobsearch.urls'
