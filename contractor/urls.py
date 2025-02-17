55bafb5de5150f5290a2e8feaa0dfdf44aee591b
Adam Campbell <adamcampbelldev@gmail.com>
2025-02-17 09:18:20 +0000

    various fixes

--- contractor/urls.py
+++ contractor/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     #path('<int:contractor_id>/', views.contractor_profile, name='contractor_profile'),
-    path('<int:user_id>/', views.contractor_detail, name="contractordetail"), #Added by Anish
+    path('<int:user_profile_id>/', views.contractor_detail, name="contractordetail"), #Added by Anish
     path('become-contractor/', views.become_contractor, name='become_contractor'),
     path('contractor/<slug:slug>/edit_review/<int:review_id>/', views.review_edit, name='review_edit'),
     path('contractor/<int:review_id>/edit/', views.review_edit, name='edit_review'),
