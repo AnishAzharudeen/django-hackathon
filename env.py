9f6fbd8323e67543c401c06fd1adf7626d882033
Azharudeen M <fatima.anish@gmail.com>
2025-02-17 09:25:34 +0000

    Collectstatic

--- env.py
+++ env.py
@@ -2,3 +2,6 @@ import os
 
 os.environ.setdefault(
     "DATABASE_URL", "postgresql://neondb_owner:z39cKviZdTNJ@ep-red-cherry-a2mpiua3.eu-central-1.aws.neon.tech/stunt_sheep_bony_696207")
+
+os.environ.setdefault(
+"CLOUDINARY_URL", "cloudinary://276878672784657:9GCVTT2xLSxvPvbtL-PNELDBjnk@ddvkbpjqa")
--- jobsearch/settings.py
+++ jobsearch/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
+    'cloudinary',
     'crispy_forms',
     'client',
     'contractor',
